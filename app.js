8352d191ae38012498f0d1e3d1c216673b01b8d9
Halle Harris <halleharris@gmx.com>
2023-03-03 16:55:18 -0500

    Passport and Token-Based Authentication

--- app.js
+++ app.js
@@ -22,8 +22,14 @@ const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var app = express();
 
@@ -34,7 +40,21 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -43,6 +63,21 @@ app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
+      err.status = 401;
+      return next(err);
+  } else {
+      return next();
+  }
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
